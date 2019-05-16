d6580c39d306fd3c5c6fb7d0e1520942769c4095
naoto.sasaki <kukandenpan@gmail.com>
2019-05-16 11:46:29 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'toppages/index'
+  root to: 'toppages#index'
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
+
   resources :tasks
   
   #resources :messagesは下記の意
