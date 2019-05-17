dc2b50f8d7788ff50db91ecfa35d4686ab2ea1db
naoto.sasaki <kukandenpan@gmail.com>
2019-05-17 14:10:26 +0000

    toppages_controller modified warning

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,6 +1,6 @@
 class ToppagesController < ApplicationController
   def index
-      if logged_in?
+    if logged_in?
       @task = current_user.tasks.build  # form_with 用
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
     end
